b2ba74e6d2e7c95701ac323a3d636cb8df83e7ca
Tigee1 <155129227+Tigee1@users.noreply.github.com>
2023-12-28 23:37:09 -0700

    Update DragonWarriorUI.js

--- DragonWarriorUI.js
+++ DragonWarriorUI.js
@@ -1,7 +1,7 @@
-const iFrame = document.getElementById("gameWindow").contentWindow.document.getElementById("gameWorldMap").style;
+const iFrame = document.getElementById("gameWindow").contentWindow; //.document.getElementById("gameWorldMap").style;
 const northButton = document.getElementById("North");
 
-
+alert(iFrame)
 function moveNorth()
 {
 iFrame.transform = "translateY(-50px)";
