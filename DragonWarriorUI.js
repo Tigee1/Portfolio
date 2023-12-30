939e47593ef424ccbc56bc7f166a5f0df91cadc7
Tigee1 <155129227+Tigee1@users.noreply.github.com>
2023-12-29 17:41:17 -0700

    Update DragonWarriorUI.js

--- DragonWarriorUI.js
+++ DragonWarriorUI.js
@@ -1,16 +1,14 @@
-const iFrame = document.getElementById("gameWindow").contentWindow;
-
-iFrame.document.getElementById("gameWorldMap").addEventListener("load", myFunction);
-
+const iFrame = document.getElementById("gameWindow");
 const northButton = document.getElementById("North");
 
+iFrame.addEventListener("load", () => 
+{  
+  const iFrameContext = iFrame.contentWindow;  
+  const gameScreen = iFrameContext.document.getElementById("gameWorldMap"); 
+});
 
 function moveNorth()
 {
 gameScreen.transform = "translateY(-50px)";
 } 
 
-function myFunction()
-{
-    alert("h")
-}
