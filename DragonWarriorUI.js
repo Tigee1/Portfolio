616f29791740d60d9ff61e0619f0e1cffd9a56ca
Tigee1 <155129227+Tigee1@users.noreply.github.com>
2023-12-30 18:43:17 -0700

    Update DragonWarriorUI.js

--- DragonWarriorUI.js
+++ DragonWarriorUI.js
@@ -1,5 +1,8 @@
 const iFrame = document.getElementById("gameWindow").contentWindow;
 const northButton = document.getElementById("North");
+let x = 0;
+let y = 0;
+position = `Position: (${x}, ${y})`
 
   iFrame.addEventListener("load", () => 
 {  
@@ -12,24 +15,32 @@ const northButton = document.getElementById("North");
 
 function moveNorth()
 {
+    y = y + 1;
     gameScreen.transform = gameScreen.transform + "translateY(16px)";
-    characterSprite.src = "Sprites/CharacterUp.png";  
+    characterSprite.src = "Sprites/CharacterUp.png"; 
+    console.log(position) 
 }
  
 function moveWest()
 {
+    x = x - 1;
     gameScreen.transform = gameScreen.transform + "translateX(16px)";
     characterSprite.src = "Sprites/CharacterLeft.png"; 
+    console.log(position) 
 } 
 
 function moveEast()
 {
+    x = x + 1;
     gameScreen.transform = gameScreen.transform + "translateX(-16px)";
     characterSprite.src = "Sprites/CharacterRight.png"; 
+    console.log(position) 
 } 
 
 function moveSouth()
 {
+    y = y - 1;
     gameScreen.transform = gameScreen.transform + "translateY(-16px)";
     characterSprite.src = "Sprites/CharacterDown.png"; 
+    console.log(position) 
 } 
