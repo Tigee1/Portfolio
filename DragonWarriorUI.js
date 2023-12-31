dbf97bb4788b0031d388189a608541d5f2b5101f
Tigee1 <155129227+Tigee1@users.noreply.github.com>
2023-12-30 18:51:45 -0700

    Update DragonWarriorUI.js

--- DragonWarriorUI.js
+++ DragonWarriorUI.js
@@ -2,7 +2,8 @@ const iFrame = document.getElementById("gameWindow").contentWindow;
 const northButton = document.getElementById("North");
 let x = 0;
 let y = 0;
-position = `Position: (${x}, ${y})`
+position = `Position: (${x}, ${y})`;
+console.log(position)
 
   iFrame.addEventListener("load", () => 
 {  
@@ -16,6 +17,7 @@ position = `Position: (${x}, ${y})`
 function moveNorth()
 {
     y = y + 1;
+    position = `Position: (${x}, ${y})`;
     gameScreen.transform = gameScreen.transform + "translateY(16px)";
     characterSprite.src = "Sprites/CharacterUp.png"; 
     console.log(position) 
@@ -24,6 +26,7 @@ function moveNorth()
 function moveWest()
 {
     x = x - 1;
+    position = `Position: (${x}, ${y})`;
     gameScreen.transform = gameScreen.transform + "translateX(16px)";
     characterSprite.src = "Sprites/CharacterLeft.png"; 
     console.log(position) 
@@ -32,6 +35,7 @@ function moveWest()
 function moveEast()
 {
     x = x + 1;
+    position = `Position: (${x}, ${y})`;
     gameScreen.transform = gameScreen.transform + "translateX(-16px)";
     characterSprite.src = "Sprites/CharacterRight.png"; 
     console.log(position) 
@@ -40,6 +44,7 @@ function moveEast()
 function moveSouth()
 {
     y = y - 1;
+    position = `Position: (${x}, ${y})`;
     gameScreen.transform = gameScreen.transform + "translateY(-16px)";
     characterSprite.src = "Sprites/CharacterDown.png"; 
     console.log(position) 
